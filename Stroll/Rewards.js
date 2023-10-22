6232a9ce4962eac6913d82f070b47a0a893e6da0
AksharChunduri <60073360+AksharChunduri@users.noreply.github.com>
2023-10-21 20:06:57 -0500

    update

--- Stroll/Rewards.js
+++ Stroll/Rewards.js
@@ -1,37 +1,29 @@
 import * as React from 'react';
 import { View, Text} from 'react-native';
-import { useTable, useGlobalFilter, useAsyncDebounce } from 'react-table' 
 import './Rewards.css'
 
-const coupons = [
+var coupons = [
     { store: "Walmart", code: "HELLO", amount: "10%", exp: "1/1/2024" },
-    { store: "McDonalds", code: "TEEMO", amount: "15%", exp: "1/2/2024" },
+    { store: "McDonalds", code: "FREE", amount: "15%", exp: "1/2/2024" },
     { store: "Target", code: "STROLL", amount: "25%", exp: "1/3/2025" },
 ]
+
+function pressed(){
+  alert('Scan this code');
+}
 
 const Rewards = () => {
     
     return (
-        <View>
-            <table>
-                <tr>
-                    <th>Store</th>
-                    <th>Code</th>
-                    <th>Amount</th>
-                    <th>Expiration Date</th>
-                </tr>
-                {coupons.map((val, key) => {
-                    return (
-                        <tr key={key}>
-                            <td>{val.store}</td>
-                            <td>{val.code}</td>
-                            <td>{val.amount}</td>
-                            <td>{val.exp}</td>
-                        </tr>
-                    )
-                })}
-            </table>
-        </View>
+        <div>
+      {coupons.map(((item) => (
+        <div key={item.id} className="post">
+          <h3>{item.store} - {item.amount} Off</h3>
+          <p>Code: {item.code}<button onClick={pressed}>REDEEM</button></p>
+          <p>Expires: {item.exp}</p>
+        </div>
+      )))}
+    </div>
     )
 }
 
