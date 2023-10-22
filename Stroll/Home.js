9c66086e11f1b51ca77499a1fc1dcb13f8f40178
shreyasuratran <shreya.suratran@gmail.com>
2023-10-22 08:13:03 -0500

    reward and home

--- Stroll/Home.js
+++ Stroll/Home.js
@@ -1,27 +1,30 @@
-import React, { useState, useEffect } from 'react';
-import { View, Text, TextInput, Button, StyleSheet, TouchableOpacity } from 'react-native';
+import React from 'react';
+import { View, Text, Button, StyleSheet } from 'react-native';
 import { useNavigation } from '@react-navigation/native';
 
 const Home = () => {
   const navigation = useNavigation();
 
-
   const handleSearch = () => {
-      // Navigate to the MapNavigation screen and pass the zip code as a parameter
-      navigation.navigate('MapNavigation');
+    // Navigate to the MapNavigation screen
+    navigation.navigate('MapNavigation');
   };
 
-  const findCurrentLocation = () => {
-        // Navigate to the MapNavigation screen and pass the latitude and longitude as parameters
-        navigation.navigate('MapNavigation', );
-      
+  const handleFindRewards = () => {
+    // Navigate to the Rewards screen
+    navigation.navigate('Rewards');
   };
 
   return (
     <View style={styles.container}>
       <Text style={styles.heading}>Start Your Stroll</Text>
       <View style={styles.optionContainer}>
-        <Button title="Find My Current Location" onPress={findCurrentLocation} color="indigo" />
+        <View style={styles.buttonBox}>
+          <Button title="Find My Current Location" onPress={handleSearch} color="indigo" />
+        </View>
+        <View style={styles.buttonBox}>
+          <Button title="Find Rewards" onPress={handleFindRewards} color="green" />
+        </View>
       </View>
     </View>
   );
@@ -44,13 +47,11 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     margin: 20,
   },
-  input: {
-    width: '80%',
-    height: 40,
-    borderColor: 'dimgray',
-    borderWidth: 1,
-    marginBottom: 10,
-    paddingLeft: 10,
+  buttonBox: {
+    backgroundColor: 'white',
+    borderRadius: 10,
+    padding: 10,
+    margin: 5,
   },
 });
 
--- Stroll/Rewards.js
+++ Stroll/Rewards.js
@@ -1,46 +1,61 @@
 import * as React from 'react';
 import { View, Text, StyleSheet, Button} from 'react-native';
-// import './Rewards.css'
 
 var coupons = [
     { store: "Walmart", code: "HELLO", amount: "10%", exp: "1/1/2024" },
     { store: "McDonalds", code: "FREE", amount: "15%", exp: "1/2/2024" },
     { store: "Target", code: "STROLL", amount: "25%", exp: "1/3/2025" },
-]
-
-function pressed(){
-  alert('Scan this code');
-}
+];
 
 const Rewards = () => {
     
     return (
-        coupons.map((item) => (
-        <View style = { styles.view }>
-          <Text>{item.store} - {item.amount} Off</Text>
-          <Text>Code: {item.code}<Button 
-            style = {styles.btn}
-            onClick={pressed}
-            title = "REDEEM"
-            /></Text>
-          <Text>Expires: {item.exp}</Text>
-        </View>
-      ))
-    
-    )
+        coupons.map((item, index) => (
+            <View key={index} style={styles.view}>
+                <Text style={styles.store}>{item.store} - {item.amount} Off</Text>
+                <Text style={styles.code}>Code: {item.code}</Text>
+                <Button
+                    title="REDEEM"
+                    onPress={pressed}
+                    color="rgb(0, 122, 33)"
+                />
+                <Text style={styles.exp}>Expires: {item.exp}</Text>
+            </View>
+        ))
+    );
 }
 
-export default Rewards;
+function pressed() {
+    alert('Scan this code');
+}
 
 const styles = StyleSheet.create({
     view: {
-        color: 'rgb(255, 255, 255)',
-        backgroundColor: 'rgb(0, 122, 33)',
-        margin: 2,
-        padding: 1,
-        fontFamily: "Arial"
+        backgroundColor: 'rgb(230, 230, 230)',
+        margin: 8,
+        padding: 10,
+        borderRadius: 8,
+        shadowColor: 'black',
+        shadowOffset: { width: 0, height: 2 },
+        shadowOpacity: 0.2,
+        shadowRadius: 4,
+        elevation: 4,
     },
-    btn: {
-      color: 'rgb(255, 255, 255)',
+    store: {
+        fontSize: 18,
+        fontWeight: 'bold',
+        marginBottom: 8,
+        color: 'rgb(56, 48, 76)',
     },
-  });
+    code: {
+        fontSize: 16,
+        marginBottom: 8,
+        color: 'rgb(56, 48, 76)',
+    },
+    exp: {
+        fontSize: 16,
+        color: 'rgb(56, 48, 76)',
+    },
+});
+
+export default Rewards;
