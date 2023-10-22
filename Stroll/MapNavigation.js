5d83f2de373815c1cf8ef864c62f88198779f232
Shreyas-Pattabi <spattabik@gmail.com>
2023-10-21 20:03:08 -0500

    Google Maps

--- Stroll/MapNavigation.js
+++ Stroll/MapNavigation.js
@@ -1,20 +1,33 @@
 import * as React from 'react';
-import { View, Text } from 'react-native'
- import MapView, {Marker} from 'react-native-maps';
+import { StyleSheet, View, Text } from 'react-native'
+import MapView, { PROVIDER_GOOGLE } from 'react-native-maps';
 
 const MapNavigation = () => {
     return (
-      <View>
-      <MapView
-              style={styles.map}
-              initialRegion={{
-                latitude: 37.78825,
-                longitude: -122.4324,
-                latitudeDelta: 0.0922,
-                longitudeDelta: 0.0421,
-              }}
-              />
+      <View style = {styles.container}>
+        <MapView
+          style = {styles.map}
+          provider = {PROVIDER_GOOGLE}
+          initialRegion={{
+          latitude: 37.78825,
+          longitude: -122.4324,
+          latitudeDelta: 0.0922,
+          longitudeDelta: 0.0421,
+          }}
+        />
       </View>
     );
   };
+
   export default MapNavigation;
+
+  const styles = StyleSheet.create({
+    container: {
+      flex: 1,
+    },
+    map: {
+      width: '100%',
+      height: '100%',
+    },
+  });
+
